18141e18e97d6e5705c2085bf520b4ca0418cba1
songxl <1229348133@qq.com>
2018-10-30 07:29:24 +0800

    "2018。10.30 创建一个类"

---
+++ src/main/java/com/songxl/test5/TesztSong.java
@@ -0,0 +1,12 @@
+package com.songxl.test5;
+
+/**
+ * @date: 2018/10/30
+ * @Version:1.0
+ * @Description:
+ */
+public class TesztSong {
+    public static void main(String[] args) {
+        System.out.println("fds");
+    }
+}
