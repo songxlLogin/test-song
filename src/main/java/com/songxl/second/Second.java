be25ea67dbfab609b595eebc6f7f088a0aa2e0c5
songxl <18625962213@163.com>
2019-08-06 23:02:23 +0800

    add second class

---
+++ src/main/java/com/songxl/second/Second.java
@@ -0,0 +1,17 @@
+package com.songxl.second;
+
+import org.junit.Test;
+
+/**
+ * @Auther: songxl
+ * @Date: 2019-8-6 22:57
+ * @Description:
+ * @Version V1.0
+ */
+public class Second {
+
+    @Test
+    public void testSecond(){
+        System.out.println("this second record");
+    }
+}
