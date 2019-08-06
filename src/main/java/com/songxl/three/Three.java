e289e0ac902e42bd4bf5c0034aa514b74212ea20
songxl <18625962213@163.com>
2019-08-06 23:29:16 +0800

    add three class

---
+++ src/main/java/com/songxl/three/Three.java
@@ -0,0 +1,16 @@
+package com.songxl.three;
+
+import org.junit.Test;
+
+/**
+ * @Auther: songxl
+ * @Date: 2019-8-6 23:28
+ * @Description:
+ * @Version V1.0
+ */
+public class Three {
+    @Test
+    public void threeTest(){
+        System.out.println("this is test three");
+    }
+}
