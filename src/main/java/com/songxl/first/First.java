7956419d8f9152638eccd735bba27aa641dd8ad0
songxl <18625962213@163.com>
2019-08-06 22:53:43 +0800

    测试版本回退，这是first文件

---
+++ src/main/java/com/songxl/first/First.java
@@ -0,0 +1,17 @@
+package com.songxl.first;
+
+import org.junit.Test;
+
+/**
+ * @Auther: songxl
+ * @Date: 2019-8-6 22:33
+ * @Description:
+ * @Version V1.0
+ */
+public class First {
+
+    @Test
+    public void testFirst(){
+        System.out.println("this first record");
+    }
+}
