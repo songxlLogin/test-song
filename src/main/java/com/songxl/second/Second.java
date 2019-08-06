ca761ccf72d4e49a45e3d2f42c7e265412829470
songxl <18625962213@163.com>
2019-08-06 23:24:55 +0800

    add second class（soft方式版本回退）

---
+++ src/main/java/com/songxl/second/Second.java
@@ -0,0 +1,19 @@
+package com.songxl.second;
+
+import org.junit.Test;
+
+/**
+ * @Auther: songxl
+ * @Date: 2019-8-6 22:57
+ * @Description:
+ * @Version V1.0
+ */
+public class Second {
+
+    @Test
+    public void testSecond(){
+        System.out.println("this second record");
+
+        System.out.println("this is soft 版本回退方式。文件回退到暂存区");
+    }
+}
